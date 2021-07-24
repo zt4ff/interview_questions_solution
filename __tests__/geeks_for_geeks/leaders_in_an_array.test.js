88292372d833889c250cb523e404663c805b73e7
kayode <segunkayode00@gmail.com>
2021-07-24 19:32:18 +0100

    leaders in array solution

--- __tests__/geeks_for_geeks/leaders_in_an_array.test.js
+++ __tests__/geeks_for_geeks/leaders_in_an_array.test.js
@@ -1,5 +1,12 @@
-const solution = require("../../geeks_for_geeks/leaders_in_an_array");
+const {
+  solution1,
+  solution2,
+} = require("../../geeks_for_geeks/leaders_in_an_array");
 
-test("test leaders in an array", () => {
-  expect(solution([1, 2, 3, 4, 0], 5)).toMatch([4, 0]);
+test("test leaders in an array - solution 1", () => {
+  expect(solution1([1, 2, 3, 4, 0], 5)).toEqual([4, 0]);
+});
+
+test("test leaders in an array - solution 2", () => {
+  expect(solution2([1, 2, 3, 4, 0], 5)).toEqual([4, 0]);
 });
--- geeks_for_geeks/leaders_in_an_array.js
+++ geeks_for_geeks/leaders_in_an_array.js
@@ -1,13 +1,34 @@
 // Article can be found here: https://www.geeksforgeeks.org/leaders-in-an-array/
 
-const solution = (arr, n) => {
+// Solution 1 - simple or brute force method
+// Time complexity: O(n * n)
+const solution1 = (arr, n) => {
   let ans = [];
   for (let i = 0; i < n; i++) {
     for (let j = i + 1; j < n; j++) {
-      if (arr[j] > arr[i]) return;
+      if (arr[j] >= arr[i]) break;
       ans.push(arr[i]);
     }
   }
+  ans.push(arr[n - 1]);
+  return ans;
 };
 
-export default solution;
+// solution 2 - max from right
+// Time Complexity: O(n)
+const solution2 = (arr, n) => {
+  let max_number = arr[n - 1];
+  let ans = [max_number];
+  for (let i = n - 2; i >= 0; i--) {
+    // check if the next right number is greater than the max_number
+    if (arr[i] > max_number) {
+      max_number = arr[i];
+      ans.unshift(max_number);
+    }
+  }
+
+  return ans;
+};
+
+exports.solution1 = solution1;
+exports.solution2 = solution2;
