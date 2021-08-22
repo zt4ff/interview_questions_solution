451a98f809d93caa754809dd2772af29d07052db
kayode <segunkayode00@gmail.com>
2021-08-22 23:04:50 +0100

    linked list middle node

--- __tests__/geeks_for_geeks/linkedlist_middle_test.py
+++ __tests__/geeks_for_geeks/linkedlist_middle_test.py
@@ -1,6 +1,25 @@
 import unittest
-
-class MiddleLinkedInTest(unittest.TestCase):
-
+import sys
+sys.path.insert(0, "/home/kayode/Documents/Learning/interview_questions/geeks_for_geeks")
+from linkedlist_middle import SinglyLinkedList
+class TestMiddleLinkedList(unittest.TestCase):
     def setUp(self) -> None:
-        self.ll_one
+        self.ll_one = SinglyLinkedList("1")
+        self.ll_one.add("2")
+        self.ll_one.add("3")
+        self.ll_one.add("4")
+        self.ll_one.add("5")
+        self.ll_one.add("6")
+        self.ll_one.add("7")
+        self.ll_one.add("8")
+
+    def test_print_function(self):
+        self.assertEqual(self.ll_one.printList(), "8 7 6 5 4 3 2 1")
+
+    def test_middle_node(self):
+        self.assertEqual(self.ll_one.printMiddle(), "4")
+        
+
+
+if __name__ == "__main__":
+    unittest.main()
