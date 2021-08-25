add50dac0bb0a5d28ca15f8817be9e4a6f67644f
kayode <segunkayode00@gmail.com>
2021-08-26 00:55:37 +0100

    queue array implemenation python

--- data_structures/queue_implementation.py
+++ data_structures/queue_implementation.py
@@ -1,4 +1,13 @@
-"Implemetation using a fixed capacity Array"
+"""
+Implementation using static data structures of array
+If we are actually remove the first item from memory, array would 
+have to shift the whole items forwards which is a time complexity of
+O(n), but can change the pointer to the next item while the dequeued 
+item is nullified.
+The con of this method is that if the array capacity is 30, you can only
+make 30 insertions no matter the number of dequeues hence you get a 
+constant time complexity of O(1) in all operations
+"""
 class Queue(object):
     def __init__(self, capacity) -> None:
         self.Q = [None] * capacity
@@ -11,3 +20,33 @@ class Queue(object):
 
     def isEmpty(self):
         return self.size == 0
+
+    def enqueue(self, item):
+        if self.isFull():
+            print("queue is full")
+            return
+        self.rear = (self.rear + 1) % self.capacity
+        self.Q[self.rear] = item
+        self.size += 1
+
+    def que_rear(self):
+        if self.isEmpty():
+            print("queue is empty")
+            return
+        return self.Q[self.rear]
+
+    def dequeue(self):
+        if self.isEmpty():
+            print("queue is empty")
+            return
+        self.front = (self.front + 1) % self.capacity
+        self.size -= 1
+
+    def que_front(self):
+        if self.isEmpty():
+            print("queue is empty")
+            return
+        return self.Q[self.front]
+
+    def total(self):
+        return self.Q
